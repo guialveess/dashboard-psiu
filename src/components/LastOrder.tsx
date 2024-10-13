0251e88266104f818331756d98ef4f3fbe4b1aa5
Guilherme Alves <97guilherme.alves@gmail.com>
2024-10-13 16:41:25 -0300

    socket io status

--- src/components/LastOrder.tsx
+++ src/components/LastOrder.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import DataTable from "./DataTable";
@@ -5,14 +6,16 @@ import { ColumnDef } from "@tanstack/react-table";
 import Link from "next/link";
 import { Order } from "../../types";
 import { io, Socket } from "socket.io-client";
+
 import { useToast } from "../components/hooks/use-toast";
+
+const socket: Socket = io("https://api-psiu-1.onrender.com");
 
 export default function LastOrder() {
   const [orders, setOrders] = useState<Order[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
-  const { toast } = useToast(); // Hook para exibir o toast
+  const { toast } = useToast();
 
-  // Função para buscar os pedidos da API usando Axios
   const fetchOrders = async () => {
     setLoading(true);
     try {
@@ -27,53 +30,114 @@ export default function LastOrder() {
     }
   };
 
+  const updateOrderStatus = async (orderId: string, newStatus: string) => {
+    console.log(`Atualizando status do pedido ${orderId} para ${newStatus}`);
+    try {
+      // Atualiza o status no servidor
+      await axios.patch(`https://api-psiu-1.onrender.com/orders/${orderId}`, {
+        status: newStatus,
+      });
+
+      // Emite um evento de atualização de pedido através do socket
+      socket.emit("ORDER_UPDATED", { orderId, newStatus });
+
+      // Atualiza o estado local
+      setOrders((prevOrders) =>
+        prevOrders.map((order) =>
+          order.id === orderId ? { ...order, status: newStatus } : order
+        )
+      );
+
+      // Corrigido o uso do toast para sucesso
+      toast(
+        `Status do pedido atualizado para "${newStatus}"`,
+        {
+          type: "success",
+        },
+        {
+          description: "O status do pedido foi atualizado com sucesso.",
+          duration: 5000,
+        }
+      );
+    } catch (error) {
+      console.error("Erro ao atualizar o status do pedido:", error);
+
+      // Corrigido o uso do toast para erro, agora dentro do catch
+      toast(
+        "Erro ao atualizar o status do pedido",
+        {
+          type: "error",
+        },
+        {
+          description:
+            "Ocorreu um erro ao tentar atualizar o status do pedido.",
+          duration: 5000,
+        }
+      );
+    }
+  };
+
   useEffect(() => {
-    // Conectar ao servidor Socket.io
     const socket: Socket = io("https://api-psiu-1.onrender.com");
 
-    // Função para tocar o som
+    socket.on("connect", () => {
+      console.log("Socket.IO conectado");
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Socket.IO desconectado");
+    });
+
     const playSound = () => {
       const audio = new Audio("/sounds/new-order.mp3");
       audio.play();
     };
 
-    // Evento quando um novo pedido for recebido
     socket.on("NEW_ORDER", (newOrder: Order) => {
-      playSound();
       console.log("Novo pedido recebido:", newOrder);
-
-      // Exibir o toast quando um novo pedido for recebido
+      playSound();
       toast(
-        `Novo pedido recebido`, // Primeiro argumento: mensagem
-        { type: "info" }, // Segundo argumento: objeto com propriedades do toast
+        `Novo pedido recebido`,
+        { type: "info" },
         {
-          // Terceiro argumento: objeto com outras propriedades
           description: `Pedido #${newOrder.id} recebido com sucesso!`,
           duration: 5000,
         }
       );
-      // Adicionar o novo pedido no topo da lista
+
       setOrders((prevOrders) => {
         const existingOrder = prevOrders.find(
           (order) => order.id === newOrder.id
         );
         if (existingOrder) {
-          // Se o pedido já existir, atualizá-lo
           return prevOrders.map((order) =>
             order.id === newOrder.id ? newOrder : order
           );
         } else {
-          // Se for um novo pedido, adicioná-lo no início da lista
           return [newOrder, ...prevOrders];
         }
       });
+    });
+
+    socket.on("ORDER_UPDATED", ({ orderId, newStatus }) => {
+      console.log(`Pedido atualizado: ${orderId}, Novo status: ${newStatus}`);
+      setOrders((prevOrders) =>
+        prevOrders.map((order) =>
+          order.id === orderId ? { ...order, status: newStatus } : order
+        )
+      );
+    });
+
+    socket.on("confirmOrder", async ({ orderId }) => {
+      // Alterado para um status válido
+      await updateOrderStatus(orderId, "success");
+      socket.emit("ORDER_UPDATED", { orderId, status: "success" });
     });
 
     fetchOrders();
 
     return () => {
       socket.disconnect();
-      console.log("Desconectado do servidor Socket.io");
     };
   }, [toast]);
 
@@ -92,7 +156,7 @@ export default function LastOrder() {
       header: "Table Number",
       cell: ({ row }) => (
         <span className="text-[#0D062D]">
-          {row.getValue("tablenumber") ?? "N/A"}
+          {row.getValue<string>("tablenumber") ?? "N/A"}
         </span>
       ),
     },
@@ -100,8 +164,8 @@ export default function LastOrder() {
       accessorKey: "status",
       header: "Status",
       cell: ({ row }) => {
-        const status = row.getValue("status") || "pending";
-        const statusText = status === "success" ? "Paid" : "pending";
+        const status = row.getValue<string>("status") || "pending";
+        const statusText = status === "success" ? "Paid" : status;
         const statusClass =
           status === "success" ? "text-[#34CAA5]" : "text-[#ED544E]";
         return <p className={statusClass}>{statusText}</p>;
@@ -119,7 +183,7 @@ export default function LastOrder() {
       header: "Observations",
       cell: ({ row }) => (
         <span className="text-[#737373]">
-          {row.getValue("observations") || "N/A"}
+          {row.getValue<string>("observations") || "N/A"}
         </span>
       ),
     },
@@ -138,7 +202,7 @@ export default function LastOrder() {
       header: "Total Price",
       cell: ({ row }) => (
         <p className="text-[#0D062D] font-semibold">
-          ${(row.getValue("totalPrice") as number).toFixed(2)}
+          ${(row.getValue<number>("totalPrice") as number).toFixed(2)}
         </p>
       ),
     },
@@ -153,6 +217,26 @@ export default function LastOrder() {
           View
         </Link>
       ),
+    },
+    {
+      accessorKey: "actions",
+      header: "Ações",
+      cell: ({ row }) => {
+        const orderId = row.getValue<string>("id");
+        return (
+          <div>
+            <button onClick={() => updateOrderStatus(orderId, "pending")}>
+              Preparando
+            </button>
+            <button onClick={() => updateOrderStatus(orderId, "success")}>
+              Confirmado
+            </button>
+            <button onClick={() => updateOrderStatus(orderId, "failed")}>
+              Cancelado
+            </button>
+          </div>
+        );
+      },
     },
   ];
 
